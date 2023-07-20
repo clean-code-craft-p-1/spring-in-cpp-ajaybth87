2894d3f5e0f5d28e7aa1ac4fddc857960dfba101
Ajay Kumar <ajay.kumar_9@philips.com>
2023-07-20 19:04:10 +0530

    Initial change to test

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,90 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+using namespace std;
+
+
+Statistics::Stats Statistics::ComputeStatistics(const std::vector<double>& Container ) {
+   Statistics::Stats obj;
+   if (!Container.empty()) {
+       auto iter1 = std::max_element(Container.begin(), Container.end());
+       obj.max = *iter1;
+       auto iter2 = std::min_element(Container.begin(), Container.end());
+       obj.min = *iter2;
+       obj.average = getAverage(Container);
+   }
+   else
+   {
+       obj.max = NAN;
+       obj.min = NAN;
+       obj.average = NAN;
+   }
+   return obj;
+}
+
+EmailAlert::EmailAlert()
+{
+    maxThreshold = 0;
+    emailSent = false;
+}
+
+void EmailAlert::checkAndAlert(const std::vector<double>& Container) 
+{
+    for (auto itr = Container.begin();itr != Container.end();itr++)
+    {
+        if (*itr > maxThreshold) {
+            emailSent = true;
+            break;
+        }
+    }
+
+}
+
+void EmailAlert::setThreshold(double Thresold)
+{
+    maxThreshold = Thresold;
+}
+
+
+LEDAlert::LEDAlert()
+{
+    maxThreshold = 0;
+    ledGlows = false;
+}
+
+void LEDAlert::checkAndAlert(const std::vector<double>& Container)
+{
+    for (auto itr = Container.begin();itr != Container.end();itr++)
+    {
+        if (*itr > maxThreshold) {
+            ledGlows = true;
+            break;
+        }
+    }
+}
+
+void LEDAlert::setThreshold(double Thresold)
+{
+    maxThreshold = Thresold;
+}
+
+StatsAlerter::StatsAlerter()
+{
+    alerts = { nullptr };
+}
+
+StatsAlerter::StatsAlerter(double thresold, std::vector<IAlerter*> container)
+{
+    alerts = container;
+    for (auto itr = alerts.begin();itr != alerts.end();itr++)
+    {
+        (*itr)->setThreshold(thresold);
+    }
+}
+
+void StatsAlerter::checkAndAlert(const std::vector<double>& Container)
+{
+    for (auto itr = alerts.begin();itr != alerts.end();itr++)
+    {
+        (*itr)->checkAndAlert(Container);
+    }
 }
--- stats.h
+++ stats.h
@@ -1,7 +1,64 @@
+
+#include<iostream>
 #include <vector>
+#include<algorithm>
+#include <numeric>
+
 
 namespace Statistics {
-    // define the Stats structure here. See the tests to infer its properties
-    
-    Stats ComputeStatistics(const std::vector<___>& );
+    struct Stats
+    {
+       double average;
+       double max;
+       double min;
+    };
+
+    Stats ComputeStatistics(const std::vector<double>& );
+
+    template<typename T>
+    double getAverage(std::vector<T> const& v) {
+        if (v.empty()) {
+            return 0;
+        }
+        return std::accumulate(v.begin(), v.end(), 0.0) / v.size();
+    }
 }
+
+class IAlerter
+{
+public:
+    double maxThreshold;
+public:
+    virtual void checkAndAlert(const std::vector<double>&) = 0;
+    virtual void setThreshold(double) = 0;
+};
+
+class EmailAlert :public IAlerter
+{
+public:
+    bool emailSent;
+    public:
+        EmailAlert();
+        void checkAndAlert(const std::vector<double>& Container) override;
+        void setThreshold(double Thresold) override;
+};
+
+class LEDAlert :public IAlerter
+{
+public:
+    bool ledGlows;
+public:
+    LEDAlert();
+    void checkAndAlert(const std::vector<double>& Container) override;
+    void setThreshold(double Thresold) override;
+};
+
+
+class StatsAlerter
+{
+    std::vector<IAlerter*> alerts;
+public:
+    StatsAlerter();
+    StatsAlerter(double, std::vector<IAlerter*>);
+    void checkAndAlert(const std::vector<double>& Container);
+};
